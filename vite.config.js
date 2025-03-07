8282d21da96f21cd8c905ac54ca3e063af03ab52
Qaiser Ejaz <qaiserejaz125@gmail.com>
2025-03-07 09:02:04 +0500

    Add GitHub Actions workflow

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/washershub-web-portal/',
-  server: {
-    port: 3000,
-    strictPort: true
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  }
 })
